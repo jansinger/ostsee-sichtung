0c20b7fad77cf7fedbbf1c7f98892a1d324f1a36
Jan Singer <jan.singer@aoe.com>
2025-08-12 23:03:06 +0200

    fix: drizzle exclude postgis tables

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!process.env.DATABASE_POSTGRES_URL) throw new Error('DATABASE_POSTGRES_URL is not set');
 
 export default defineConfig({
 	out: './drizzle',
 	schema: './src/lib/server/db/schema.ts',
 	dialect: 'postgresql',
-	dbCredentials: { url: process.env.DATABASE_URL },
+	dbCredentials: { url: process.env.DATABASE_POSTGRES_URL },
 	verbose: true,
-	strict: true
+	strict: true,
+	tablesFilter: [
+		'!pg_stat_statements',
+		'!pg_stat_statements_info',
+		'!spatial_ref_sys',
+		'!geography_columns',
+		'!geometry_columns'
+	]
 });
