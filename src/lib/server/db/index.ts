1e2f5449d0ac54d7a494ba4e4843260113214c2a
Jan Singer <jan.singer@aoe.com>
2025-08-12 15:55:48 +0200

    fix: database url

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,10 @@
+import { env } from '$env/dynamic/private';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
-import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!env.DATABASE_POSTGRES_URL) throw new Error('DATABASE_URL is not set');
 
-const client = postgres(env.DATABASE_URL);
+const client = postgres(env.DATABASE_POSTGRES_URL);
 
 export const db = drizzle(client, { schema });
