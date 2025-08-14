7c45d4c8088abe60679f59ed2b4f99472ddff7d6
Jan Singer <jan.singer@aoe.com>
2025-08-14 17:35:52 +0200

    fix: do not generate static assets

--- vite.config.ts
+++ vite.config.ts
@@ -19,20 +19,6 @@ export default defineConfig({
 		host: 'localhost',
 		port: 4000
 	},
-	build: {
-		rollupOptions: {
-			output: {
-				assetFileNames: (assetInfo) => {
-					// Stelle sicher, dass Schriftarten in den assets-Ordner kopiert werden
-					if (assetInfo.names && assetInfo.names[0] && /\.(woff2?|eot|ttf|otf)$/.test(assetInfo.names[0])) {
-						return 'assets/[name]-[hash][extname]';
-					}
-					return 'assets/[name]-[hash][extname]';
-				}
-			}
-		}
-	},
-	assetsInclude: ['**/*.woff', '**/*.woff2', '**/*.eot', '**/*.ttf', '**/*.otf'],
 	test: {
 		projects: [
 			{
